9f844b98f15700a6c4502eb75d96a6e71aec539d
NikCoh <nik.coh@student.um.si>
2025-11-29 19:19:02 +0100

    Added basic room background, floor and lighting.

--- src/scenes/ClassroomScene.ts
+++ src/scenes/ClassroomScene.ts
@@ -6,8 +6,40 @@ export default class ClassroomScene extends Phaser.Scene {
     }
 
     create() {
+        const width = this.scale.width;
+        const height = this.scale.height;
+
+        this.createBackground(width, height);
+
         this.scale.on('resize', this.handleResize, this);
     }
+
+    private createBackground(width: number, height: number) {
+        const floorHeight = height * 0.2;
+        const floorGradient = this.add.rectangle(
+            width / 2,
+            height - floorHeight / 2,
+            width,
+            floorHeight,
+            0x607d8b
+        );
+
+        const wallHeight = height * 0.8;
+        const wall = this.add.rectangle(
+            width / 2,
+            wallHeight / 2,
+            width,
+            wallHeight,
+            0xc5ccd1
+        );
+
+        const light1 = this.add.rectangle(width * 0.28, height * 0.05, width * 0.15, height * 0.01, 0xffffff, 0.4);
+        light1.setBlendMode(Phaser.BlendModes.ADD);
+
+        const light2 = this.add.rectangle(width * 0.72, height * 0.05, width * 0.15, height * 0.01, 0xffffff, 0.4);
+        light2.setBlendMode(Phaser.BlendModes.ADD);
+    }
+
 
     private handleResize() {
         this.scene.restart();
